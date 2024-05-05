a4894cd4a1973e1c5e1a039c0bad4ef72aec9649
Vinícius Galvão <vinicius.gcarvalho@ufpe.br>
2024-05-05 17:16:41 -0300

    Início da aplicação

---
+++ src/main/java/br/com/demo/dflix/DflixApplication.java
@@ -0,0 +1,14 @@
+package br.com.demo.dflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DflixApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/br/com/demo/dflix/DflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.demo.dflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
