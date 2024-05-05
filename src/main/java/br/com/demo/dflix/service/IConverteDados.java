208f3ed03e0b95f5f8d8ceaa366a41286ba46060
Vinícius Galvão <vinicius.gcarvalho@ufpe.br>
2024-05-05 18:44:33 -0300

    Adicionado conversor de dados da API

---
+++ src/main/java/br/com/demo/dflix/service/IConverteDados.java
@@ -0,0 +1,9 @@
+package br.com.demo.dflix.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+public interface IConverteDados {
+    <T> T obterDados(String json, Class<T> classe) throws JsonMappingException, JsonProcessingException;
+
+}
