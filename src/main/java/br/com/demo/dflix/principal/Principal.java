03e0a1fff1d85e57eeb851e7363d7cf24eacfcc3
herrittt <herrittt@VGC.>
2024-12-11 22:01:43 -0300

    Adicionado consumo do BD

--- src/main/java/br/com/demo/dflix/principal/Principal.java
+++ src/main/java/br/com/demo/dflix/principal/Principal.java
@@ -96,7 +96,7 @@ public class Principal {
 
     private void listarSeriesBuscadas() {
         List<Serie> series = new ArrayList<>();
-        series = dadosSeries.stream().map(d-> new Serie(d)).collect(Collectors.toList());
+        series = serieRepository.findAll();
         series.stream().sorted(Comparator.comparing(Serie::getGenero)).forEach(System.out::println);
     }
 }
