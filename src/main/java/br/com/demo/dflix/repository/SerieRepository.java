4e6a86372b389ff05835867cf059116776fac288
herrittt <vgalvao7@gmail.com>
2025-02-05 21:17:17 -0300

    Adicionado buscas para repositório de séries

--- src/main/java/br/com/demo/dflix/repository/SerieRepository.java
+++ src/main/java/br/com/demo/dflix/repository/SerieRepository.java
@@ -1,11 +1,17 @@
 package br.com.demo.dflix.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.demo.dflix.model.Serie;
+import enums.Categoria;
 
 public interface SerieRepository extends JpaRepository<Serie, Long>{
     Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
+
+    Optional<List<Serie>> findByGeneroEquals(Categoria genero);
+
+    Optional<List<Serie>> findByAvaliacaoGreaterThanEqual(Double avaliacao);
 }
